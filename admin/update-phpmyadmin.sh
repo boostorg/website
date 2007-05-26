8afb820d082d5106127452f6dd482ee144ab1ee9
Rene Rivera <grafikrobot@gmail.com>
2007-05-26 20:16:50 +0000

    Nothing to see here. Move along.
    [SVN r4287]

--- admin/update-phpmyadmin.sh
+++ admin/update-phpmyadmin.sh
@@ -3,7 +3,7 @@ set -e
 
 PHPMYADMIN_VERSION='2.10.1'
 
-phpmyadmin="phpMyAdmin-$PHPMYADMIN_VERSION-all-languages"
+phpmyadmin="phpMyAdmin-${PHPMYADMIN_VERSION}-all-languages"
 
 ##
 #  Fetch all the parts, and extract.
@@ -25,7 +25,7 @@ blowfish_secret=`date | md5sum | head -c 32`
 cat - >>config.inc.php <<PHP
 <?php
 \$cfg['PmaAbsoluteUri'] = 'http://beta.boost.org/admin/db/';
-\$cfg['blowfish_secret'] = '$blowfish_secret';
+\$cfg['blowfish_secret'] = '${blowfish_secret}';
 \$i = 0;
 \$i++;
 \$cfg['Servers'][\$i] = \$cfg['Servers'][0];
