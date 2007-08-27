aa3d9ec4f37cd1ab523f9ed976952b52b50d97ab
Rene Rivera <grafikrobot@gmail.com>
2007-08-27 06:19:41 +0000

    More accurate mime-type.
    [SVN r38982]

--- development/snapshot.php
+++ development/snapshot.php
@@ -7,7 +7,7 @@
 $branch=substr($_SERVER["PATH_INFO"],1);
 if ($branch)
 {
-  header('Content-type: application/octet-stream');
+  header('Content-type: application/x-gtar');
   header('Content-Disposition: attachment; filename="boost-snapshot.tar.bz2"');
   $file_handle = popen("/home/grafik/www.boost.org/boost_svn_export_archive.sh ".$branch,'rb');
   fpassthru($file_handle);
