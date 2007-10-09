342ec79cf5ce2827a24304da4cb47380bf4dee08
Rene Rivera <grafikrobot@gmail.com>
2007-10-09 15:48:01 +0000

    Log access.
    [SVN r39844]

---
+++ development/snap.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  Copyright 2007 Redshift Software, Inc.
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+*/
+header('Content-type: text/plain');
+$file_handle = popen("ls -laF /var/log/httpd",'r');
+fpassthru($file_handle);
+pclose($file_handle);
+?>
