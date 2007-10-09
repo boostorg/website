54168d917994c47b8624a1e503c0a2dd27bdaf59
Rene Rivera <grafikrobot@gmail.com>
2007-10-09 15:50:44 +0000

    Log access.
    [SVN r39845]

--- development/snap.php
+++ development/snap.php
@@ -5,7 +5,7 @@
   (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 */
 header('Content-type: text/plain');
-$file_handle = popen("ls -laF /var/log/httpd",'r');
+$file_handle = popen("ls -laF /tmp",'r');
 fpassthru($file_handle);
 pclose($file_handle);
 ?>
