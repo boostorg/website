331a2c7ebc968e0146b13cd9c9919dfc3688f357
Rene Rivera <grafikrobot@gmail.com>
2007-10-11 04:16:45 +0000

    Debug version of tarball generator.
    [SVN r39916]

---
+++ development/snapshot_debug.php
@@ -0,0 +1,16 @@
+<?php
+/*
+  Copyright 2007 Redshift Software, Inc.
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+*/
+$branch=substr($_SERVER["PATH_INFO"],1);
+if ($branch)
+{
+  header('Content-type: application/x-bzip2');
+  header('Content-Disposition: attachment; filename="boost-snapshot.tar.bz2"');
+  $file_handle = popen("/home/grafik/www.boost.org/boost_svn_export_archive.sh ".$branch,'rb');
+  fpassthru($file_handle);
+  pclose($file_handle);
+}
+?>
