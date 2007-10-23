1f840467d98922482e2abfc4e2d820f9a2796afc
Rene Rivera <grafikrobot@gmail.com>
2007-10-23 03:18:40 +0000

    Exclude regression test results from web checks.
    [SVN r40319]

--- common/code/webcheck.sh
+++ common/code/webcheck.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 /usr/bin/python24 /home/grafik/www.boost.org/testing/webcheck.py \
- "--yank=http://beta.boost.org/doc/libs/" \
+ "--yank=http://beta.boost.org/doc/libs" \
+ "--yank=http://beta.boost.org/development/tests" \
  "--base-only" \
  "--quiet" \
  "--output=/home/grafik/www.boost.org/testing/webcheck" \
