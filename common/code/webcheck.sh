2d49c19fcde5ba70c2e791efd1a9d84a499e6bee
Rene Rivera <grafikrobot@gmail.com>
2007-10-23 03:09:15 +0000

    Web site checking with webcheck.
    [SVN r40317]

--- common/code/webcheck.sh
+++ common/code/webcheck.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 /usr/bin/python24 /home/grafik/www.boost.org/testing/webcheck.py \
- --yank=http://beta.boost.org/doc/libs/ \
- --base-only \
- --quiet \
- --output=/home/grafik/www.boost.org/testing/webcheck \
- --force
+ "--yank=http://beta.boost.org/doc/libs/" \
+ "--base-only" \
+ "--quiet" \
+ "--output=/home/grafik/www.boost.org/testing/webcheck" \
+ "--force"
