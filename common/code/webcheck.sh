682a1ec6963b88d51afd6bd75e5da64467969535
Rene Rivera <grafikrobot@gmail.com>
2007-10-23 04:09:57 +0000

    Prevent web check of the web check results.
    [SVN r40331]

--- common/code/webcheck.sh
+++ common/code/webcheck.sh
@@ -3,6 +3,7 @@
 /usr/bin/python24 /home/grafik/www.boost.org/testing/webcheck.py \
  "--yank=http://beta.boost.org/doc/libs" \
  "--yank=http://beta.boost.org/development/tests" \
+ "--yank=http://beta.boost.org/development/webcheck" \
  "--base-only" \
  "--quiet" \
  "--output=/home/grafik/www.boost.org/testing/webcheck" \
