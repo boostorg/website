d232d83cef08510fa10de8b8d2a9186dd4be36ac
Rene Rivera <grafikrobot@gmail.com>
2007-12-05 21:12:23 +0000

    Move the rest of the server only code to the various update scripts.
    [SVN r41760]

--- common/code/update-6x6.sh
+++ common/code/update-6x6.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-cd ${HOME}/www.boost.org
-
-wget -q -O gmane.comp.lib.boost.announce.rss http://rss.gmane.org/topics/excerpts/gmane.comp.lib.boost.announce
-wget -q -O gmane.comp.lib.boost.build.rss http://rss.gmane.org/topics/excerpts/gmane.comp.lib.boost.build
-wget -q -O gmane.comp.lib.boost.devel.rss http://rss.gmane.org/topics/excerpts/gmane.comp.lib.boost.devel
-wget -q -O gmane.comp.lib.boost.documentation.rss http://rss.gmane.org/topics/excerpts/gmane.comp.lib.boost.documentation
-wget -q -O gmane.comp.lib.boost.interest.rss http://rss.gmane.org/topics/excerpts/gmane.comp.lib.boost.interest
-wget -q -O gmane.comp.lib.boost.testing.rss http://rss.gmane.org/topics/excerpts/gmane.comp.lib.boost.testing
-wget -q -O gmane.comp.lib.boost.user.rss http://rss.gmane.org/topics/excerpts/gmane.comp.lib.boost.user
+cd ${HOME}/www.boost.org/testing
+for result in `find incoming -mmin +5 -name '*.zip'` ; do
+  f=`basename ${result}`
+  mv -f live/${f} old
+  mv -f ${result} live
+  rm -f old/${f}
+done
