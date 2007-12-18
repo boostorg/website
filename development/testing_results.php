7a371e62232d5a020cdc1b42cfd9406e3650a534
Rene Rivera <grafikrobot@gmail.com>
2007-12-18 18:51:46 +0000

    Stupid Rene forgot to change one reference to archive_file :-(
    [SVN r42142]

--- development/testing_results.php
+++ development/testing_results.php
@@ -1,7 +1,7 @@
 <?php
 require_once(dirname(__FILE__) . '/../common/code/boost_archive.php');
 
-$_file = new archive_file(
+$_file = new boost_archive(
   '/^[\/]([^\/]+)[\/](.*)$/',$_SERVER["PATH_INFO"],array(),
   true, // we always want raw output
   false, // the result zips don't have the tag subdir
