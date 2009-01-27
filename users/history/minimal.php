9e0d19a3b5faee3feb57d910a81c4af4cae9da54
Daniel James <daniel@calamity.org.uk>
2009-01-27 21:24:02 +0000

    Use a minimal 404 page in minimal.php if the entry isn't found.
    [SVN r50826]

--- users/history/minimal.php
+++ users/history/minimal.php
@@ -3,9 +3,7 @@ require_once(dirname(__FILE__) . '/../../common/code/boost_feed.php');
 $_history = new boost_feed(dirname(__FILE__) . '/../../feed/history.rss', '/users/history');
 $_guid = basename($_SERVER["PATH_INFO"]);
 if(!isset($_history->db[$_guid])) {
-    require_once(dirname(__FILE__) . '/../../common/code/boost_error_page.php');
-    error_page_404();
-    exit(0);
+    header('HTTP/1.0 404 Not Found');
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
@@ -19,8 +17,14 @@ if(!isset($_history->db[$_guid])) {
 </head>
 
 <body>
+<?php if(!isset($_history->db[$_guid])) : ?>
+  <h2>404 Not Found</h2>
+
+  <p>The entry you requested could not be found.</p>
+<?php else : ?>
   <h2><?php print $_history->db[$_guid]['title']; ?></h2>
 
   <?php print $_history->db[$_guid]['description']; ?>
+<?php endif ?>
 </body>
 </html>
