60da9ff5a5954a290214ffbbb629031b71751ca2
Daniel James <daniel@calamity.org.uk>
2009-02-08 14:15:02 +0000

    1.38 docs.
    [SVN r51087]

--- doc/libraries.php
+++ doc/libraries.php
@@ -95,7 +95,7 @@ function libref($lib)
     }
     else
     {
-      $docref = '/doc/libs/1_37_0/'.$lib['documentation'];
+      $docref = '/doc/libs/1_38_0/'.$lib['documentation'];
     }
     print '<a href="'.$docref.'">'.($lib['name'] ? $lib['name'] : $lib['key']).'</a>';
   }
