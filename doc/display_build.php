964ee81d7530cd6ad69b809e2d24a7d7d21c1854
Vladimir Prus <ghost@cs.msu.su>
2009-06-07 09:41:09 +0000

    Add Google Analytics tracking code for Boost.Build docs.
    [SVN r53706]

--- doc/display_build.php
+++ doc/display_build.php
@@ -72,5 +72,11 @@ div.sidebar {
 
     <div class="clear"></div>
   </div>
+  <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+  </script>
+  <script type="text/javascript">
+  _uacct = "UA-2917240-2";
+  urchinTracker();
+  </script>  
 </body>
 </html><?php } ?>
