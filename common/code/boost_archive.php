e3c05d7bed6e246af0033ed484c3355ce16d99d7
Daniel James <daniel@calamity.org.uk>
2011-08-11 20:59:04 +0000

    Website: Add noindex to quickbook and directory pages.
    [SVN r73676]

--- common/code/boost_archive.php
+++ common/code/boost_archive.php
@@ -102,7 +102,8 @@ function display_from_archive(
     // Choose filter to use
 
     $info_map = array_merge($content_map, array(
-        array('@.*@','@[.](txt|py|rst|jam|v2|bat|sh|xml|toyxml|qbk)$@i','text','text/plain'),
+        array('@.*@','@[.](txt|py|rst|jam|v2|bat|sh|xml|toyxml)$@i','text','text/plain'),
+        array('@.*@','@[.](qbk|quickbook)$@i','qbk','text/plain'),
         array('@.*@','@[.](c|h|cpp|hpp)$@i','cpp','text/plain'),
         array('@.*@','@[.]png$@i','raw','image/png'),
         array('@.*@','@[.]gif$@i','raw','image/gif'),
@@ -245,18 +246,19 @@ function conditional_get($last_modified)
 
 function boost_archive_render_callbacks($content, $params) {
     $version_title =
-    	str_replace('_', ' ',
-    		preg_replace('@(?<=\d)_(?=\d)@', '.',
-    			ucwords($params['version'])));
+        str_replace('_', ' ',
+            preg_replace('@(?<=\d)_(?=\d)@', '.',
+                ucwords($params['version'])));
 
     $charset = $params['charset'] ? $params['charset'] : 'us-ascii';
     $title = $params['title'] ? "$params[title] - $version_title" : 'Boost C++ Libraries';
 
+    $head = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=${charset}\" />\n";
 
-    $head = <<<HTML
-<meta http-equiv="Content-Type" content="text/html; charset=${charset}" />
-<title>${title}</title>
-HTML;
+    if (!empty($params['noindex'])
+        $head .= "<meta name=\"robots\" content=\"noindex\">\n";
+
+    $head .= "<title>${title}</title>";
 
     return Array(
         'head' => $head,
@@ -271,6 +273,7 @@ function display_dir($params, $dir)
     $title = htmlentities("Index listing for $params[key]");
 
     $params['title'] = $title;
+    $params['noindex'] = true;
     
     $content = "<h3>$title</h3>\n<ul>\n";
 
