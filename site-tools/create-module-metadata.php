89ed945ffc1d2d5c37c9813fbe9918181114ef11
Daniel James <daniel@calamity.org.uk>
2014-07-13 11:21:44 +0100

    Get module paths from git when creating metadata.

--- site-tools/create-module-metadata.php
+++ site-tools/create-module-metadata.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once(__DIR__ . '/../common/code/boost_libraries.php');
+require_once(__DIR__ . '/boost_super_project.php');
 
 function main() {
     $args = $_SERVER['argv'];
@@ -13,8 +14,11 @@ function main() {
             exit(1);
     }
 
-    $libs = boost_libraries::from_xml_file(__DIR__ . '/../doc/libraries.xml');
-    $library_details = $libs->get_for_version(BoostVersion::develop());
+    $library_details =
+        boost_libraries::from_xml_file(__DIR__ . '/../doc/libraries.xml')
+            ->get_for_version(BoostVersion::develop());
+    $git_submodules = (new BoostSuperProject($boost_root))->get_modules();
+
 
     // Get the library data, so that it can be updated with maintainers.
     // In case you're wondering why the result from get_for_version doesn't
@@ -64,9 +68,14 @@ function main() {
     foreach ($library_details as $library) {
         $module = $library['module'];
 
+        if (!isset($git_submodules[$module])) {
+            echo "Unknown module: {$module}\n";
+            continue;
+        }
+
         if (isset($library['documentation'])) {
             $doc_url = $library['documentation'];
-            $module_base = "libs/$module";
+            $module_base = $git_submodules[$module]['path'];
 
             if ($doc_url == $module_base) {
                 $doc_url = '';
@@ -93,12 +102,12 @@ function main() {
 
     foreach ($libraries_by_module as $module => $libraries) {
         $module_libraries = boost_libraries::from_array($libraries);
-        $module_dir = "$boost_root/libs/$module";
+        $module_dir = "{$boost_root}/{$git_submodules[$module]['path']}";
         $meta_dir = "$module_dir/meta";
         $meta_file = "$module_dir/meta/libraries.json";
 
         if (!is_dir($module_dir)) {
-            echo "Module doesn't exist: $module\n";
+            echo "Module '$module' doesn't exist at '$module_dir'\n";
             continue;
         }
 
