75d70fec56c8498c00eb85d4bd9614a791216799
Daniel James <daniel@calamity.org.uk>
2014-08-07 20:34:31 +0100

    Setup test_libraries properly.

--- common/code/test/test_libraries.php
+++ common/code/test/test_libraries.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__.'/../boost_libraries.php');
+require_once(__DIR__.'/../boost.php');
 
 $libraries = BoostLibraries::from_xml('<?xml version="1.0" encoding="US-ASCII"?>
 <boost xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
