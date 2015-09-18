b17e5d64bc102fcf65f38ce4d431a92d1b6610ac
Daniel James <daniel@calamity.org.uk>
2015-09-18 13:29:45 +0100

    More detail in unzip error message.

--- common/code/boost_archive.php
+++ common/code/boost_archive.php
@@ -332,7 +332,7 @@ function display_raw_file($params, $type)
         // be getting them for legitimate files.
 
         if($exit_status > 3)
-            echo 'Error extracting file: '.unzip_error($exit_status);
+            echo 'Error extracting file: '.unzip_error($params, $exit_status);
     }
     else {
         readfile($params['file']);
@@ -353,7 +353,7 @@ function extract_file($params, &$content) {
             return true;
         }
         else {
-            $content = strstr($_SERVER['HTTP_HOST'], 'beta') ? unzip_error($exit_status) : null;
+            $content = strstr($_SERVER['HTTP_HOST'], 'beta') ? unzip_error($params, $exit_status) : null;
             return false;
         }
     }
@@ -466,24 +466,32 @@ function latest_link($params)
 
 // Return a readable error message for unzip exit state.
 
-function unzip_error($exit_status) {
+function unzip_error($params, $exit_status) {
     switch($exit_status) {
-    case 0: return 'No error.';
-    case 1: return 'One  or  more  warning  errors  were  encountered.';
-    case 2: return 'A generic error in the zipfile format was detected.';
-    case 3: return 'A severe error in the zipfile format was detected.';
-    case 4: return 'Unzip was unable to allocate memory for one or more buffers during program initialization.';
-    case 5: return 'Unzip was unable to allocate memory or unable to obtain a tty to read the decryption password(s).';
-    case 6: return 'Unzip was unable to allocate memory during decompression to disk.';
-    case 7: return 'Unzip was unable to allocate memory during in-memory decompression.';
-    case 9: return 'The specified zipfile was not found.';
-    case 10: return 'Invalid options were specified on the command line.';
-    case 11: return 'No matching files were found.';
-    case 50: return 'The disk is (or was) full during extraction.';
-    case 51: return 'The end of the ZIP archive was encountered prematurely.';
-    case 80: return 'The user aborted unzip prematurely with control-C (or similar).';
-    case 81: return 'Testing or extraction of one or more files failed due to unsupported compression methods or unsupported decryption.';
-    case 82: return 'No files were found due to bad decryption password(s).';
-    default: return 'Unknown unzip error code: ' + $exit_status;
+    case 0: $message = 'No error.'; break;
+    case 1: $message = 'One  or  more  warning  errors  were  encountered.'; break;
+    case 2: $message = 'A generic error in the zipfile format was detected.'; break;
+    case 3: $message = 'A severe error in the zipfile format was detected.'; break;
+    case 4: $message = 'Unzip was unable to allocate memory for one or more buffers during program initialization.'; break;
+    case 5: $message = 'Unzip was unable to allocate memory or unable to obtain a tty to read the decryption password(s).'; break;
+    case 6: $message = 'Unzip was unable to allocate memory during decompression to disk.'; break;
+    case 7: $message = 'Unzip was unable to allocate memory during in-memory decompression.'; break;
+    case 9:
+        $message = 'The specified zipfile was not found';
+        if (isset($params['archive']) && is_file($params['archive'])) {
+            $message .= ' <i>(the file exists, so this is probably an error reading the file)</i>';
+        }
+        $message .= '.';
+        break;
+    case 10: $message = 'Invalid options were specified on the command line.'; break;
+    case 11: $message = 'No matching files were found.'; break;
+    case 50: $message = 'The disk is (or was) full during extraction.'; break;
+    case 51: $message = 'The end of the ZIP archive was encountered prematurely.'; break;
+    case 80: $message = 'The user aborted unzip prematurely with control-C (or similar).'; break;
+    case 81: $message = 'Testing or extraction of one or more files failed due to unsupported compression methods or unsupported decryption.'; break;
+    case 82: $message = 'No files were found due to bad decryption password(s).'; break;
+    default: $message = 'Unknown unzip error code.'; break;
     }
+
+    return "Error code ".html_encode($exit_status)." - {$message}";
 }
