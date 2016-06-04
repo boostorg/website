add557ff8affa5c5cdd52f7cb671c04980861bfa
Daniel James <daniel@calamity.org.uk>
2016-06-04 17:07:23 +0100

    Do all the processing in the tasks repo.

--- webhook/github-webhook.php
+++ webhook/github-webhook.php
@@ -4,32 +4,10 @@
  *       we just want to know where to find the implementation. */
 require_once(__DIR__.'/../common/code/boost_config.php');
 
-function boost_website_webhook() {
-    /* Check the payload */
-    if (!array_key_exists('payload', $_POST)) {
-        die("Unable to find payload.\n");
-    }
-
-    $payload = $_POST['payload'];
-
-    /* Get github headers */
-    $github_headers = array();
-    foreach($_SERVER as $key => $value) {
-        if (preg_match('@^HTTP_X_GITHUB_(.*)$@', $key, $match)) {
-            $github_headers[strtolower($match[1])] = $value;
-        }
-    }
-
-    $signature = array_key_exists('HTTP_X_HUB_SIGNATURE', $_SERVER) ?
-        $_SERVER['HTTP_X_HUB_SIGNATURE'] : false;
-
-    require_once(BOOST_TASKS_DIR.'/webhook/webhook.php');
-
-    if (!array_key_exists('event', $github_headers)) {
-        die("No event header.\n");
-    }
-
-    webhook($github_headers, $payload, $signature);
+if (!defined('BOOST_TASKS_DIR')) {
+    die("Tasks directory not set.");
 }
 
-boost_website_webhook();
+require_once(BOOST_TASKS_DIR.'/webhook/webhook.php');
+
+webhook();
