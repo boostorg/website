7ee9d41e9e1ebc86c084ce6b0ed947317f814c67
Daniel James <daniel@calamity.org.uk>
2016-10-07 09:48:02 +0100

    Update release downloads from file
    Pretty rough at the moment.

--- common/code/boost_releases.php
+++ common/code/boost_releases.php
@@ -66,6 +66,71 @@ class BoostReleases {
         return $flat;
     }
 
+    // Expected format:
+    //
+    // URL
+    // (blank line)
+    // Output of sha256sum
     function loadReleaseInfo($release_details) {
+        if (!preg_match('@
+            \A
+            \s*([^\s]*)[ \t]*\n
+            [ \t]*\n
+            (.*)
+            @xs', $release_details, $matches))
+        {
+            throw new BoostException("Error parsing release details");
+        }
+
+        $download_page = $matches[1];
+        $sha256sums = explode("\n", trim($matches[2]));
+
+        // TODO: Better URL validation?
+        if (substr($download_page, -1) != '/') {
+            throw new BoostException("Release details needs to start with a directory URL");
+        }
+
+        $version = BoostVersion::from($download_page);
+        $base_version = $version->final_doc_dir();
+        $version_string = (string) $version;
+
+        $downloads = array();
+        foreach($sha256sums as $sha256sum) {
+            if (!preg_match('@^([0-9a-f]{64}) *([a-zA-Z0-9_.]*)$@', trim($sha256sum), $match)) {
+                throw new BoostException("Invalid sha256sum: {$sha256sum}");
+            }
+
+            $sha256 = $match[1];
+            $filename = $match[2];
+            $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+            $extensions = array(
+                '7z' => 'windows', 'zip' => 'windows',
+                'gz' => 'unix', 'bz2' => 'unix',
+            );
+            if (!array_key_exists($extension, $extensions)) {
+                throw new BoostException("Invalid extension: {$filename}");
+            }
+            $line_endings = $extensions[$extension];
+
+            $downloads[$extension] = array(
+                'line_endings' => $line_endings,
+                'url' => "{$download_page}{$filename}",
+                'sha256' => $sha256,
+            );
+        }
+
+        // TODO: Should probably set documentation URL before loading in the
+        //       release data, so the array keys should already exist?
+        if (!array_key_exists($base_version, $this->release_data)) {
+            $this->release_data[$base_version] = array();
+        }
+        if (!array_key_exists($version_string, $this->release_data[$base_version])) {
+            $this->release_data[$base_version][$version_string] = array(
+                'release_status' => 'dev',
+            );
+        }
+        $this->release_data[$base_version][$version_string]['download_page'] = $download_page;
+        $this->release_data[$base_version][$version_string]['downloads'] = $downloads;
     }
 }
