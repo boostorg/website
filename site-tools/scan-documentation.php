8d54f400e4728db826c16d27e20cf12956fce424
Daniel James <daniel@calamity.org.uk>
2016-11-22 09:55:06 +0000

    Fix documentation directory in scan-documentation.php

--- site-tools/scan-documentation.php
+++ site-tools/scan-documentation.php
@@ -34,7 +34,7 @@ function main() {
             // Store this somewhere?
         }
         else if (preg_match('@^(boost)_([0-9_]+)$@', $name, $match)) {
-            $releases->addDocumentation($match[1], BoostVersion::from($match[2]), "/doc/libs/{$name}");
+            $releases->addDocumentation($match[1], BoostVersion::from($match[2]), "/doc/libs/{$match[2]}/");
         }
     }
 
